f94ba5f1b97a433444648d4e337d9adda4dccc4e
iliyan marinov <ili.2@abv.bg>
2020-05-28 20:11:14 +0300

    update

--- Calendar/Calendar/Date.cpp
+++ Calendar/Calendar/Date.cpp
@@ -12,13 +12,88 @@ Date::Date(const Date& other)
 	day = other.day;
 }
 
-//bool Date::Check();//year -20
-//bool Date::operator==(const Date& other);
-//bool Date::operator!=(const Date& other);
-//bool Date::operator>(const Date& other);
-//bool Date::operator>=(const Date& other);
-//bool Date::operator<(const Date& other);
-//bool Date::operator<=(const Date& other);
+bool Date::Check()//year -20
+{
+	if ((month == 1 || month == 3 || month == 5 ||
+		month == 7 || month == 8 || month == 10 ||
+		month == 12) && (day > 0 && day < 32))
+	{
+		return true;
+	}
+	else if ((month == 4 || month == 6 ||
+		month == 9 || month == 11) &&
+		(day > 0 && day < 31))
+	{
+		return true;
+	}
+	else if (year % 4 == 0 && month == 2 &&
+		(day > 0 && day < 30))
+	{
+		return true;
+	}
+	else if (year % 4 != 0 && month == 2 &&
+		(day > 0 && day < 29))
+	{
+		return true;
+	}
+	return false;
+}
+bool Date::operator==(const Date& other)
+{
+	if (year == other.year && month == other.month && day == other.day)
+	{
+		return true;
+	}
+	return false;
+}
+bool Date::operator!=(const Date& other)
+{
+	if (*this == other)
+	{
+		return false;
+	}
+	return true;
+}
+bool Date::operator>(const Date& other)
+{
+	if (year > other.year)
+	{
+		return true;
+	}
+	else if (year == other.year && month > other.month)
+	{
+		return true;
+	}
+	else if (year == other.year && month == other.month && day > other.day)
+	{
+		return true;
+	}
+	return false;
+}
+bool Date::operator>=(const Date& other)
+{
+	if (*this > other || *this == other)
+	{
+		return true;
+	}
+	return false;
+}
+bool Date::operator<(const Date& other)
+{
+	if (*this >= other)
+	{
+		return false;
+	}
+	return true;
+}
+bool Date::operator<=(const Date& other)
+{
+	if (*this < other || *this == other)
+	{
+		return true;
+	}
+	return false;
+}
 
 std::istream& operator>>(std::istream& in, Date& obj)
 {
