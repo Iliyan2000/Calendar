88ef251a4cec462ff59f46555c1ef40f90346f76
iliyan marinov <ili.2@abv.bg>
2020-05-28 20:29:49 +0300

    update

--- Calendar/Calendar/Booked.cpp
+++ Calendar/Calendar/Booked.cpp
@@ -1,1 +1,23 @@
 #include "Booked.h"
+
+Booked::Booked(const Booked& other)
+{
+	date = other.date;
+	start_time = other.start_time;
+	end_time = other.end_time;
+	name = other.name;
+	note = other.note;
+}
+
+void Insert_and_sort(Vector<Booked> vec, const Booked& obj)
+{
+	size_t i = 0;
+	size_t length = vec.size();
+	for (i = 0; i < length; i++)
+	{
+		if (true)
+		{
+
+		}
+	}
+}
--- Calendar/Calendar/Booked.h
+++ Calendar/Calendar/Booked.h
@@ -1,5 +1,30 @@
 #pragma once
+#include "Time.h"
+#include "Date.h"
+#include "Vector.h"
+
 class Booked
 {
-};
+public:
+	Booked() = default;
+	Booked(const Booked& other);
 
+	inline Date getDate() const { return date; }
+	inline Time getStartTime() const { return start_time; }
+	inline Time getEndTime() const { return end_time; }
+	inline std::string getName() const { return name; }
+	inline std::string getNote() const { return note; }
+	void setDate(const Date& _date) { date = _date; }
+	void setStartTime(const Time& _start_time) { start_time = _start_time; }
+	void setEndTime(const Time& _end_time) { end_time = _end_time; }
+	void setName(const std::string _name) { name = _name; }
+	void setNote(const std::string _note) { note = _note; }
+
+private:
+	Date date;
+	Time start_time;
+	Time end_time;
+	std::string name;
+	std::string note;
+};
+void Insert_and_sort(Vector<Booked> vec, const Booked& obj);
